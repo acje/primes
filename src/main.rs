4e516d992ceebffe6b3a604ecb976616b46b0937
Anders Jensen <anders.c.jensen@gmail.com>
2020-03-07 21:44:49 +0100

    naming convention

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,46 @@ fn main() {
     let now = Instant::now();
     const LIMIT: u64 = 100_000;
     let mut vec = vec![2, 3, 5, 7];
-    let mut _count = 4u64;
-    let mut _candidate = 11u64;
-    let _upper_divisor = (LIMIT as f64).sqrt() as u64 + 1;
+    let mut count = 4u64;
+    let mut candidate = 11u64;
+    let upper_divisor = (LIMIT as f64).sqrt() as u64 + 1;
 
     loop {
-        if check_for_prime(_candidate, &vec) {
-            _count += 1;
-            if _candidate < _upper_divisor {
-                vec.push(_candidate);
+        if check_for_prime(candidate, &vec) {
+            count += 1;
+            if candidate < upper_divisor {
+                vec.push(candidate);
             }
         }
-        _candidate += 2; // skip odd numbers
-        if _candidate >= LIMIT {
+        candidate += 2; // skip odd numbers
+        if candidate >= LIMIT {
             break;
         }
         // second half of unroled loop
-        if check_for_prime(_candidate, &vec) {
-            _count += 1;
-            vec.push(_candidate);
+        if check_for_prime(candidate, &vec) {
+            count += 1;
+            vec.push(candidate);
         }
-        _candidate += 4; // skip every second odd number as it divides by 3
-        if _candidate >= LIMIT {
+        candidate += 4; // skip every second odd number as it divides by 3
+        if candidate >= LIMIT {
             break;
         }
     }
     println!(
-        "All primes smaller than {0} computed in {1} seconds",
+        "All primes smaller than {0} computed in {1} microseconds",
         LIMIT,
-        now.elapsed().as_secs()
+        now.elapsed().as_micros()
     );
-    println!("Count: {}", _count);
+    println!("Count: {}", count);
 }
 
-fn check_for_prime(_pc: u64, vec: &[u64]) -> bool {
-    let _upper_limit = (_pc as f64).sqrt() as u64 + 1;
+fn check_for_prime(pc: u64, vec: &[u64]) -> bool {
+    let upper_limit = (pc as f64).sqrt() as u64 + 1;
     for element in vec.iter() {
-        if _pc % element == 0 {
+        if pc % element == 0 {
             return false;
         }
-        if element >= &_upper_limit {
+        if element >= &upper_limit {
             return true;
         }
     }
